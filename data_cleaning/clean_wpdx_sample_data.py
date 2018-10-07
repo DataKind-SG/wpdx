1d97c464d69304944c94854f62c8afac9edd4c45
Avaneesh Thautam <avaneesh.t@gmail.com>
2018-10-07 11:17:27 +0800

    convert install year to int & throw an error message  if install year > current year

--- data_cleaning/clean_wpdx_sample_data.py
+++ data_cleaning/clean_wpdx_sample_data.py
@@ -1,5 +1,5 @@
 import csv
-
+import datetime
 
 def clean_columns(input_file, output_file):
     with open(input_file) as csvfile, open(output_file, 'wt') as writer:
@@ -24,6 +24,31 @@ def clean_col_country_name(input_data):
     """
     return input_data
 
+def clean_col_install_year(input_data):
+    """
+    
+    Clean values in column: "install_year"
+    Trello card: https://trello.com/c/HHzNs0hS/1-column-countryname
+    
+    1. Convert value to int
+        This will not work if input_data is a list.
+        
+    2. Check if Install year is > current year 
+        -> print ("Error - Install year is in the future")
+    
+    """
+
+    input_data = int(input_data)
+
+    # check if input_data is < current year
+    current_year = datetime.date.today().year
+    if input_data > current_year:
+        print("Error - Install year is in the future")
+    return input_data
+
+
 
 if __name__ == '__main__':
     clean_columns('wpdx_sample_data.csv', 'cleaned_wpdx_sample_data.csv')
+
+    
--- data_cleaning/test_clean_wpdx_sample_data.py
+++ data_cleaning/test_clean_wpdx_sample_data.py
@@ -7,3 +7,12 @@ def test_clean_col_country_name():
     Test the cleaning for column: "country_name"
     """
     assert clean_wpdx_sample_data.clean_col_country_name('NA') == 'NA'
+
+def test_clean_col_install_year():
+    """
+    Test the cleaning for column: "install_year"
+    s"""
+    assert clean_wpdx_sample_data.clean_col_install_year('2012') == 2012
+    assert clean_wpdx_sample_data.clean_col_install_year(2012.32) == 2012
+    assert clean_wpdx_sample_data.clean_col_install_year('2020') == 2020
+    
